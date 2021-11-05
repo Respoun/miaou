3aae797f87d096dd8ecfec88f7537d1ed2492238
AntoineHaller <antoine.haller@ynov.com>
2021-11-05 12:28:56 +0100

    Dernier petit test

--- miaou/lib/main.dart
+++ miaou/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Miaou',
+      title: 'Miaou l\'application',
       theme: ThemeData(
 
         primarySwatch: Colors.blue,
